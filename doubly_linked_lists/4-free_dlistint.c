04ac22af1f8cf8b32fdccb8364ebddd0ef4cb609
yunusemretokyay1 <yunusemretokyay@gmail.com>
2023-11-29 00:47:36 -0800

    Free list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * free_dlistint
+  * 
+  * @head: The head of the doubly linked list
+  * 
+  *
+  * 
+  */
+void free_dlistint(dlistint_t *head);
+{
+	dlistint_t *current = NULL, *new_node = NULL;
+while(current!=NULL)
+ {
+  next->current=next;
+  free(current);
+  current=next;
+ }
+ }
+
